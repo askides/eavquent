37c5f4444116e15f594f2d8d1c4c43525a82654c
Renato Pozzi <r.pozzi@bluecube.it>
2021-03-03 21:22:18 +0100

    New preparation

--- src/EavquentServiceProvider.php
+++ src/EavquentServiceProvider.php
@@ -1,36 +1,18 @@
 <?php
 
-namespace Rennypoz\Eavquent;
+namespace ItsRennyMan\Eavquent;
 
 use Illuminate\Support\ServiceProvider;
 
 class EavquentServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap the application services.
-     */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            // Publish configurations
-            // $this->publishes([
-            //     __DIR__.'/../config/config.php' => config_path('eavquent.php'),
-            // ], 'config');
-
-            // Registering package commands.
-            // $this->commands([]);
-        }
+        //
     }
 
-    /**
-     * Register the application services.
-     */
     public function register()
     {
-        // Automatically apply the package configuration
-        // $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'eavquent');
-
-        // Register the main class to use with the facade
         $this->app->singleton('eavquent', function () {
             return new Eavquent;
         });
